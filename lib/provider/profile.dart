268ca635f7cb3a3fd7c1c024ccbd6d0497fb3097
iori <iori@iori.dev>
2021-12-31 14:35:27 +0900

    add autodispose

--- lib/provider/profile.dart
+++ lib/provider/profile.dart
@@ -3,6 +3,6 @@ import 'package:solarized_twitter/model/profile.dart';
 import 'package:solarized_twitter/model/request/twitter_request.dart';
 
 final profileProvider =
-    FutureProvider.family<Profile, String>((ref, userId) async {
+    FutureProvider.autoDispose.family<Profile, String>((ref, userId) async {
   return getUserProfile(userId);
 });
