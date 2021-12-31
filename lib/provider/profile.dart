d1df5566d3734f9d6660992ce3870a67909f5bc0
iori <iori@iori.dev>
2021-12-31 14:33:23 +0900

    use  FutureProvider.family

--- lib/provider/profile.dart
+++ lib/provider/profile.dart
@@ -1,4 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:solarized_twitter/model/profile.dart';
+import 'package:solarized_twitter/model/request/twitter_request.dart';
 
-final profileProvider = StateProvider<Profile?>((ref) => null);
+final profileProvider =
+    FutureProvider.family<Profile, String>((ref, userId) async {
+  return getUserProfile(userId);
+});
